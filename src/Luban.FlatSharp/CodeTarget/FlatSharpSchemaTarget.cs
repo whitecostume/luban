bcfce677d9a1bd19e2b6e044cbf0273b8381aef1
xiangchen02 <18351967270@163.com>
2024-02-18 17:57:36 +0800

    add FlatSharp

---
+++ src/Luban.FlatSharp/CodeTarget/FlatSharpSchemaTarget.cs
@@ -0,0 +1,45 @@
+﻿using Luban;
+using Luban.CodeFormat;
+using Luban.CodeTarget;
+using Luban.Defs;
+using Luban.FlatBuffers.TemplateExtensions;
+using Luban.FlatBuffers.TypeVisitors;
+using Luban.Tmpl;
+using Scriban;
+using Scriban.Runtime;
+
+
+[CodeTarget("flatsharp")]
+public class FlatSharpSchemaTarget : AllInOneTemplateCodeTargetBase
+{
+    public override string FileHeader => "";
+
+    protected override string FileSuffixName => "fbs";
+
+    protected override ICodeStyle CodeStyle => CodeFormatManager.Ins.NoneCodeStyle;
+
+    protected override string DefaultOutputFileName => "schema.fbs";
+
+    protected override void OnCreateTemplateContext(TemplateContext ctx)
+    {
+        ctx.PushGlobal(new FlatSharpTemplateExtension());
+
+        var maps = CollectKeyValueEntry(GenerationContext.Current.ExportBeans).KeyValueEntries.Values;
+        ctx.PushGlobal(new ScriptObject()
+        {
+            {"__maps", maps},
+        });
+    }
+
+    private MapKeyValueEntryCollection CollectKeyValueEntry(List<DefBean> beans)
+    {
+        var c = new MapKeyValueEntryCollection();
+
+        foreach (DefBean bean in beans)
+        {
+            CollectMapKeyValueEntriesVisitor.Ins.Accept(bean, c);
+        }
+
+        return c;
+    }
+}
